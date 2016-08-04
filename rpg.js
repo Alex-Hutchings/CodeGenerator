853bcde91cd30985c850dc2717cc4e3da92cd813
Alex-Hutchings <hutchalex@live.co.uk>
2016-08-04 16:09:39 +0100

    Add files via upload

---
+++ rpg.js
@@ -0,0 +1,55 @@
+//name="class"
+//values = Knight, Wizard, Archer, Thief
+//ids = knight, wizard, archer, thief
+function classStats(){
+if(document.getElementById("knight").checked= true){
+	var health = "100";
+	var mana = "50";
+	var dexterity = "75";
+	var intelligence = "25";
+
+	document.getElementById("health").value = health;
+	document.getElementById("mana").value = mana;
+	document.getElementById("dexterity").value = dexterity;
+	document.getElementById("intelligence").value = intelligence;
+	}
+}
+function wizardStats(){
+if(document.getElementById("wizard").checked = true){
+	var health = "75";
+	var mana = "100";
+	var dexterity = "25";
+	var intelligence = "100";
+
+	document.getElementById("health").value = health;
+	document.getElementById("mana").value = mana;
+	document.getElementById("dexterity").value = dexterity;
+	document.getElementById("intelligence").value = intelligence;
+	}
+}
+function archerStats(){
+if(document.getElementById("archer").checked = true){
+	var health = "100";
+	var mana = "25";
+	var dexterity = "50";
+	var intelligence = "25";
+
+	document.getElementById("health").value = health;
+	document.getElementById("mana").value = mana;
+	document.getElementById("dexterity").value = dexterity;
+	document.getElementById("intelligence").value = intelligence;
+	}
+}
+function thiefStats(){
+if(document.getElementById("thief").checked = true){
+	var health = "100";
+	var mana = "25";
+	var dexterity = "50";
+	var intelligence = "25";
+
+	document.getElementById("health").value = health;
+	document.getElementById("mana").value = mana;
+	document.getElementById("dexterity").value = dexterity;
+	document.getElementById("intelligence").value = intelligence;
+	}
+}
