13a496c4ea86903f7f9c88d5a846d0edac78ff09
Alex-Hutchings <hutchalex@live.co.uk>
2016-08-05 14:21:49 +0100

    Add files via upload

--- rpg.js
+++ rpg.js
@@ -1,24 +1,27 @@
 //Created by codeGenerator.com
 function classStats(){
-if(document.getElementById("knight").checked= true){
 	var health = "100";
 	var mana = "50";
 	var dexterity = "75";
 	var intelligence = "25";
 
+if(document.getElementById("knight").checked= true){
 	document.getElementById("health").value = health;
 	document.getElementById("mana").value = mana;
 	document.getElementById("dexterity").value = dexterity;
-	document.getElementById("intelligence").value = intelligence;
-	}
+	document.getElementById("intelligence").value = intelligence;}
+	
+
+	if(document.getElementById("male").checked = true){
+		document.getElementById("image").style.display = "block";
+		document.getElementById("image").src = "images/knight.png";}
 }
 function wizardStats(){
-if(document.getElementById("wizard").checked = true){
 	var health = "75";
 	var mana = "100";
 	var dexterity = "25";
 	var intelligence = "100";
-
+if(document.getElementById("wizard").checked = true){
 	document.getElementById("health").value = health;
 	document.getElementById("mana").value = mana;
 	document.getElementById("dexterity").value = dexterity;
@@ -27,12 +30,11 @@ if(document.getElementById("wizard").checked = true){
 	}
 }
 function archerStats(){
-if(document.getElementById("archer").checked = true){
 	var health = "100";
 	var mana = "25";
 	var dexterity = "50";
 	var intelligence = "25";
-
+if(document.getElementById("archer").checked = true){
 	document.getElementById("health").value = health;
 	document.getElementById("mana").value = mana;
 	document.getElementById("dexterity").value = dexterity;
@@ -41,34 +43,18 @@ if(document.getElementById("archer").checked = true){
 	}
 }
 function thiefStats(){
-if(document.getElementById("thief").checked = true){
 	var health = "100";
 	var mana = "25";
 	var dexterity = "50";
 	var intelligence = "25";
-
+if(document.getElementById("thief").checked = true){
 	document.getElementById("health").value = health;
 	document.getElementById("mana").value = mana;
 	document.getElementById("dexterity").value = dexterity;
 	document.getElementById("intelligence").value = intelligence;
 
 	}
+
 }
 
-function genderChoice(){
-	if(document.getElementById("male").checked = true){
-		document.getElementById("image").style.display = "block";
-		document.getElementById("image").src = "images/knight.png";}
 
-	else if(document.getElementById("knight").checked = true && document.getElementById("female").checked = true){
-		document.getElementById("image").style.display = "block";
-		document.getElementById("image").src = "images/knightF.png";}
-
-	else if(document.getElementById("wizard").checked = true && document.getElementById("male").checked = true){
-		document.getElementById("image").style.display = "block";
-		document.getElementById("image").src = "images/wizardM.png";}
-
-	else if(document.getElementById("wizard").checked = true && document.getElementById("female").checked = true){
-		document.getElementById("image").style.display = "block";
-		document.getElementById("image").src = "images/wizardF.png";}
-}
